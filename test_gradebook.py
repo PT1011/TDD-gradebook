51e5eee637fdce2bfbccd0bfa27186dd89802f75
Parth Thite <parthpthite@gmail.com>
2026-04-22 08:06:54 -0400

    fix test_is_passing_invalid_type

--- test_gradebook.py
+++ test_gradebook.py
@@ -21,6 +21,6 @@ def test_is_passing_true():
 def test_is_passing_false():
     assert is_passing(45) == False
 
-def test_is_passing_invalid_type
+def test_is_passing_invalid_type:
     with pytest.raises(TypeError):
         is_passing("Hello")
