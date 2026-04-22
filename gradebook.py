f67cdef0cd27d571a942a8aa778101e4ec2fe2fb
Parth Thite <parthpthite@gmail.com>
2026-04-22 08:22:09 -0400

    Refactor average function to add error handling

--- gradebook.py
+++ gradebook.py
@@ -19,6 +19,8 @@ def is_passing(score):
     return score >= 60
 
 def average(scores):
+    if len(scores) == 0:
+        raise ValueError("There must be 1 or more scores in list")
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
